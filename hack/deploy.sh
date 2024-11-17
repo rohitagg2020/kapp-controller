2b739828ca86a8e2e9fcebd32f33a0fb2c1e5d26
Rohit Aggarwal <rohit.aggarwal@broadcom.com>
2024-11-17 09:37:28 +0530

    Bumping controller runtime
    Signed-off-by: Rohit Aggarwal <rohit.aggarwal@broadcom.com>

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -5,7 +5,8 @@ set -e
 # makes the get_kappctrl_ver function available (scrapes version from git tag)
 source $(dirname "$0")/version-util.sh
 
-./hack/build.sh && ytt -f config/config -f config/values-schema.yml -f config-dev -v dev.version="$(get_kappctrl_ver)+develop"  | kbld -f- | kapp deploy -a kc -f- -c -y
+# ./hack/build.sh && ytt -f config/config -f config/values-schema.yml -f config-dev -v dev.version="$(get_kappctrl_ver)+develop"  | kbld -f- | kapp deploy -a kc -f- -c -y
 
+./hack/build.sh && ytt -f config/config -f config/values-schema.yml -f config-dev -v dev.version="$(get_kappctrl_ver)+develop"  | kbld -f- > output.yml
 source ./hack/secretgen-controller.sh
 deploy_secretgen-controller
